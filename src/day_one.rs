82f7c1eb884cc797c6e7c035b1fb1a9632368077
Charlie <30825964+Vxlnce@users.noreply.github.com>
2023-12-04 12:22:50 +1100

    finished part 2 of day one

--- src/day_one.rs
+++ src/day_one.rs
@@ -18,6 +18,83 @@ pub fn find_calibration_sum(inp: Box<str>) -> usize {
     total
 }
 
+
+fn find_num_in_string(inp: &str) -> Option<char> {
+    static NUMBERS: [&'static str; 10] = ["zero", "one", "two", "three", "four", "five","six","seven","eight","nine"];
+    
+    for (index, num) in NUMBERS.into_iter().enumerate() {
+        if num.len() > inp.len() {
+            continue;
+        }
+
+        if inp.contains(num) {
+            return char::from_digit(index as u32, 10)
+        }
+    }
+    None
+}
+
+pub fn find_calibration_sum_with_letters(inp: Box<str>) -> usize {
+    let mut total = 0;
+
+    for s in inp.split("\n").into_iter() {
+        // match digit or character, then figure out if valid character slice
+        let mut first = '\0';
+        let mut last = '\0';
+
+        // forward loop
+        for (index, c) in s.char_indices().into_iter() {
+            match c.is_digit(10) {
+                true => {
+                    first = c;
+                    break;
+                },
+                false => {
+                    first = find_num_in_string(&s[0..=index]).unwrap_or('\0');
+                    if first != '\0' {
+                        break;
+                    }
+                },
+            }
+
+        }
+
+        // reverse loop
+        for (index, c) in s.char_indices().into_iter().rev() {
+            match c.is_digit(10) {
+                true => {
+                    last = c;
+                    break;
+                },
+                false => {
+                    last = find_num_in_string(&s[index..].chars().collect::<String>()).unwrap_or('\0');
+                    if last != '\0' {
+                        break;
+                    }
+                },
+            }
+
+        }
+
+        // some error handling
+        if first == '\0' {
+            first = '0';
+        }
+        if last == '\0' {
+            last = first;
+        }
+
+        let mut calibration_string = "".to_string();
+        calibration_string.push(first);
+        calibration_string.push(last);
+
+        total += calibration_string.parse::<usize>().unwrap_or_default();
+
+    }
+    
+    total
+}
+
 #[cfg(test)]
 mod tests {
     use std::error::Error;
@@ -27,7 +104,7 @@ mod tests {
     #[test]
     fn max_elf() -> Result<(), Box<dyn Error + 'static>> {
         let data = common::read_file("data/day_one.txt")?;
-        assert_eq!(142, super::find_calibration_sum(data));
+        assert_eq!(281, super::find_calibration_sum_with_letters(data));
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error + 'static>> {
     println!(
         "{}",
-        day_one::find_calibration_sum(common::read_file("data/day_one_submission.txt")?)
+        day_one::find_calibration_sum_with_letters(common::read_file("data/day_one_submission.txt")?)
     );
     Ok(())
 }
